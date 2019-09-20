d7eafecbbb519279c2259b6d41be004b9b4a41f9
Joe Strouth <joestrouth@curo.com>
2019-09-20 11:18:27 -0500

    feat(scoring): make survey submittable, with post-score navigation

--- idg/src/pages/scoring.tsx
+++ idg/src/pages/scoring.tsx
@@ -1,21 +1,50 @@
 /**@jsx jsx */
 import { jsx, Container, Main } from 'theme-ui'
-import { useRef, FormEventHandler } from 'react'
+import { MouseEventHandler, useState, Fragment, useEffect } from 'react'
 import { Icon } from 'c-components'
+import { navigate } from 'gatsby'
 import { DefaultLayout as Layout } from '../components/layouts'
 import { SEO } from '../components/seo'
 import { Illustration } from '../components/illustration'
+
+type Rating = 'POSITIVE' | 'NEUTRAL' | 'NEGATIVE'
+
+const ThankYouMessage = () => (
+  <p sx={{ variant: 'type.body', my: 3 }}>
+    Thank you for sharing how you feel.
+  </p>
+)
+
+function getScoreAndNavigate() {
+  if (typeof window !== 'undefined') {
+    const goodScore = confirm(
+      'Did the applicant score well?\nOK for yes, Cancel for no.'
+    )
+    if (goodScore) {
+      navigate('/link-account')
+    } else {
+      navigate('/income-details')
+    }
+  }
+}
 
 /**
  * Where applicants wait while we score them.
  */
 const ScoringPage = () => {
-  const formRef = useRef<HTMLFormElement>(null)
-  const handleSubmit: FormEventHandler = e => {
-    /* TODO: Add validation and submit navigation */
-    e.preventDefault()
-    console.log(e.target, 'form submitted')
+  const [feedback, setFeedback] = useState<Rating | null>(null)
+  const handleFeedback = (
+    rating: Rating
+  ): MouseEventHandler<HTMLButtonElement> => (): void => {
+    /* TODO: Add feedback, thank you, and submit navigation */
+    console.log(`User rated the application: ${rating}`)
+    setFeedback(rating)
   }
+  useEffect(() => {
+    if (feedback) {
+      setTimeout(getScoreAndNavigate, 1500)
+    }
+  }, [feedback])
 
   return (
     <Layout>
@@ -49,61 +78,78 @@ const ScoringPage = () => {
 
           <p sx={{ variant: 'type.body' }}>
             This might take a couple of minutes but we’ll be quick—please don’t
-            close or refresh the page! How’s the application going?
+            close or refresh the page!
           </p>
-          <p sx={{ variant: 'type.body', my: 3 }}>
-            How’s the application going?
-          </p>
-          <form
-            ref={formRef}
-            onSubmit={handleSubmit}
-            sx={{
-              display: 'flex',
-              flexFlow: 'row nowrap',
-            }}
-          >
-            <button
-              sx={{
-                variant: 'cards.radio',
-                mr: 2,
-                flex: 1,
-                display: 'flex',
-                flexFlow: 'column nowrap',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <Icon name="frown" alt="Negative:" width={24} height={24} />
-              <span sx={{ variant: 'type.body' }}>Poor</span>
-            </button>
-            <button
-              sx={{
-                variant: 'cards.radio',
-                flex: 1,
-                mr: 2,
-                display: 'flex',
-                flexFlow: 'column nowrap',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <Icon name="meh" alt="Neutral:" width={24} height={24} />
-              <span sx={{ variant: 'type.body' }}>OK</span>
-            </button>
-            <button
-              sx={{
-                variant: 'cards.radio',
-                flex: 1,
-                display: 'flex',
-                flexFlow: 'column nowrap',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <Icon name="smile" alt="Positive:" width={24} height={24} />
-              <span sx={{ variant: 'type.body' }}>Good</span>
-            </button>
-          </form>
+          {feedback ? (
+            <ThankYouMessage />
+          ) : (
+            <Fragment>
+              <p sx={{ variant: 'type.body', my: 3 }}>
+                How’s the application going?
+              </p>
+              <div
+                sx={{
+                  display: 'flex',
+                  flexFlow: 'row nowrap',
+                }}
+              >
+                <button
+                  sx={{
+                    variant: 'cards.radio',
+                    mr: 2,
+                    flex: 1,
+                    display: 'flex',
+                    flexFlow: 'column nowrap',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  onClick={handleFeedback('NEGATIVE')}
+                >
+                  <Icon
+                    name="frown"
+                    alt="Frowning face."
+                    width={24}
+                    height={24}
+                  />
+                  <span sx={{ variant: 'type.body' }}>Poor</span>
+                </button>
+                <button
+                  sx={{
+                    variant: 'cards.radio',
+                    flex: 1,
+                    mr: 2,
+                    display: 'flex',
+                    flexFlow: 'column nowrap',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  onClick={handleFeedback('NEUTRAL')}
+                >
+                  <Icon name="meh" alt="Neutral face." width={24} height={24} />
+                  <span sx={{ variant: 'type.body' }}>OK</span>
+                </button>
+                <button
+                  sx={{
+                    variant: 'cards.radio',
+                    flex: 1,
+                    display: 'flex',
+                    flexFlow: 'column nowrap',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  onClick={handleFeedback('POSITIVE')}
+                >
+                  <Icon
+                    name="smile"
+                    alt="Smiling face."
+                    width={24}
+                    height={24}
+                  />
+                  <span sx={{ variant: 'type.body' }}>Good</span>
+                </button>
+              </div>
+            </Fragment>
+          )}
         </Container>
       </Main>
     </Layout>
