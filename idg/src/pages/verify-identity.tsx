d71c8d15d31fc7a72bd2989850f5f4469ff89b63
Joe Strouth <joestrouth1@gmail.com>
2019-09-25 20:38:20 -0500

    feat(idg): use ModalLink on verify-identity

--- idg/src/pages/verify-identity.tsx
+++ idg/src/pages/verify-identity.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect, useRef } from 'react'
 import { navigate } from 'gatsby'
 import { DefaultLayout as Layout } from '../components/layouts'
 import { SEO } from '../components/seo'
-import { ModalContext } from '../contexts/modal'
+import { ModalLink } from '../components/modal-link'
 import PrivacyPolicyModal from './privacy-policy/modal'
 import { FormEventHandler } from 'react'
 
@@ -32,102 +32,76 @@ const VerifyIdentityPage = () => {
   return (
     <Layout>
       <SEO title="Verify your identity" />
-      <ModalContext.Consumer>
-        {modalDispatch => (
-          <Main>
-            <Container
-              sx={{
-                px: 3,
-                py: 4,
-                maxWidth: theme => theme.breakpoints[0],
-                display: 'flex',
-                flexFlow: 'column nowrap',
-              }}
+      <Main>
+        <Container
+          sx={{
+            px: 3,
+            py: 4,
+            maxWidth: theme => theme.breakpoints[0],
+            display: 'flex',
+            flexFlow: 'column nowrap',
+          }}
+        >
+          <header>
+            <h1 sx={{ variant: 'type.title', mb: 3 }}>Verify your identity</h1>
+          </header>
+
+          <p
+            sx={{
+              variant: 'type.body',
+              mb: 3,
+            }}
+          >
+            <Icon name="lock" alt="" width={16} height={16} sx={{ mr: 1 }} />
+            Your information is encrypted and held securely according to{' '}
+            <ModalLink modalContent={<PrivacyPolicyModal />}>
+              our privacy policy
+            </ModalLink>
+            .
+          </p>
+
+          <form
+            sx={{ display: 'flex', flexFlow: 'column nowrap' }}
+            onSubmit={handleSubmit}
+            ref={formRef}
+          >
+            <TextField
+              label="Social Security Number"
+              type="text"
+              name="ssn"
+              hint="123-45-6789"
+              pattern="[0-9-]*"
+              sx={{ mb: 3 }}
+              value={social}
+              onChange={e => setSocial(e.currentTarget.value)}
+              required
+            />
+            <TextField
+              label="Date of birth"
+              name="dob"
+              type="date"
+              sx={{ mb: 3 }}
+              value={formattedDateOfBirth}
+              onChange={e => setDateOfBirth(new Date(e.currentTarget.value))}
+              required
+            />
+            <Flex
+              onClick={() =>
+                !isValid && formRef.current && formRef.current.reportValidity()
+              }
             >
-              <header>
-                <h1 sx={{ variant: 'type.title', mb: 3 }}>
-                  Verify your identity
-                </h1>
-              </header>
-
-              <p
-                sx={{
-                  variant: 'type.body',
-                  mb: 3,
-                }}
+              <Button
+                variant="primary"
+                type="submit"
+                disabled={!isValid}
+                sx={{ flex: 1 }}
               >
-                <Icon
-                  name="lock"
-                  alt=""
-                  width={16}
-                  height={16}
-                  sx={{ mr: 1 }}
-                />
-                Your information is encrypted and held securely according to{' '}
-                <Button
-                  onClick={e => {
-                    e.preventDefault()
-                    modalDispatch({
-                      type: 'SET_CONTENT',
-                      payload: <PrivacyPolicyModal />,
-                    })
-                    modalDispatch({ type: 'OPEN' })
-                  }}
-                  variant="link"
-                >
-                  our privacy policy
-                </Button>
-                .
-              </p>
-
-              <form
-                sx={{ display: 'flex', flexFlow: 'column nowrap' }}
-                onSubmit={handleSubmit}
-                ref={formRef}
-              >
-                <TextField
-                  label="Social Security Number"
-                  type="text"
-                  name="ssn"
-                  hint="123-45-6789"
-                  pattern="[0-9-]*"
-                  sx={{ mb: 3 }}
-                  value={social}
-                  onChange={e => setSocial(e.currentTarget.value)}
-                  required
-                />
-                <TextField
-                  label="Date of birth"
-                  name="dob"
-                  type="date"
-                  sx={{ mb: 3 }}
-                  value={formattedDateOfBirth}
-                  onChange={e =>
-                    setDateOfBirth(new Date(e.currentTarget.value))
-                  }
-                  required
-                />
-                <Flex
-                  onClick={() =>
-                    !isValid &&
-                    formRef.current &&
-                    formRef.current.reportValidity()
-                  }
-                >
-                  <Button
-                    variant="primary"
-                    type="submit"
-                    disabled={!isValid}
-                    sx={{ flex: 1 }}
-                  >
-                    Next
-                  </Button>
-                </Flex>
-              </form>
-            </Container>
-          </Main>
-        )}
-      </ModalContext.Consumer>
+                Next
+              </Button>
+            </Flex>
+          </form>
+        </Container>
+      </Main>
     </Layout>
   )
 }
