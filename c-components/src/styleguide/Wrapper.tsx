27f6416b2d273b20923e7fb2e10916c247dfe47e
Joe Strouth <joestrouth@curo.com>
2019-10-03 09:45:22 -0500

    feat(styleguide): Add device width selector to examples

--- c-components/src/styleguide/Wrapper.tsx
+++ c-components/src/styleguide/Wrapper.tsx
@@ -1,22 +1,94 @@
 /**@jsx jsx */
 import { jsx, Styled, ThemeProvider } from 'theme-ui'
-import { Component } from 'react'
+import { Component, useState, HTMLAttributes, ReactNode } from 'react'
 import { defaultTheme } from '../theme'
 import 'typeface-open-sans'
 import 'normalize.css'
 
-function SpacedStack(props: React.HTMLAttributes<HTMLDivElement>) {
+const Width = {
+  'iPhone SE': 320,
+  'Galaxy S': 360,
+  iPhone: 375,
+  'iPhone Plus': 414,
+  'Android 7"': 600,
+  iPad: 768,
+  // '720p': 1280,
+  // '1080p': 1920,
+  // '1440p': 2560,
+  // '4k': 3840,
+}
+
+interface SpacedStackProps extends HTMLAttributes<HTMLDivElement> {
+  width?: number
+  children?: ReactNode
+}
+
+function SpacedStack({
+  width = Width['iPhone SE'],
+  ...props
+}: SpacedStackProps) {
   return (
     <div
       {...props}
       sx={{
         display: 'grid',
         gridGap: 16,
-        maxWidth: 360,
+        maxWidth: width,
         mx: 'auto',
         alignContent: 'stretch',
       }}
     />
+  )
+}
+
+function WidthSelector({
+  children,
+}: {
+  children: (width: number) => ReactNode
+}) {
+  const [width, setWidth] = useState<number>(Width['iPhone SE'])
+
+  return (
+    <div sx={{}}>
+      <div
+        sx={{
+          width: '100%',
+          display: 'flex',
+          flexFlow: 'row wrap',
+          justifyContent: 'center',
+          m: -1,
+          borderBottomWidth: 1,
+          borderBottomStyle: 'solid',
+          borderBottomColor: 'grays.6',
+          pb: 2,
+          mb: 3,
+        }}
+      >
+        {Object.entries(Width).map(([device, width]) => {
+          return (
+            <button
+              key={device}
+              sx={{
+                variant: 'buttons.base',
+                backgroundColor: 'transparent',
+                textTransform: 'uppercase',
+                fontFamily: 'body',
+                fontWeight: 'body',
+                fontSize: 1,
+                color: 'grays.8',
+                px: 2,
+                py: 1,
+                m: 1,
+              }}
+              onClick={() => setWidth(width)}
+            >
+              {device}
+            </button>
+          )
+        })}
+      </div>
+      {children(width)}
+    </div>
   )
 }
 
@@ -25,7 +97,11 @@ export default class Wrapper extends Component {
     return (
       <ThemeProvider theme={defaultTheme}>
         <Styled.root>
-          <SpacedStack>{this.props.children}</SpacedStack>
+          <WidthSelector>
+            {width => (
+              <SpacedStack width={width}>{this.props.children}</SpacedStack>
+            )}
+          </WidthSelector>
         </Styled.root>
       </ThemeProvider>
     )
